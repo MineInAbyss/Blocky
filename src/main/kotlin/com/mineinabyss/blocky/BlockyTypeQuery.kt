c4b35689bcb2c034981b05785406db8ea46983d1
Boy <sivertpaulsen2@gmail.com>
2022-06-17 00:40:28 +0200

    fix build issue

--- src/main/kotlin/com/mineinabyss/blocky/BlockyTypeQuery.kt
+++ src/main/kotlin/com/mineinabyss/blocky/BlockyTypeQuery.kt
@@ -1,15 +1,13 @@
 package com.mineinabyss.blocky
 
 import com.mineinabyss.blocky.BlockyTypeQuery.key
-import com.mineinabyss.blocky.components.BlockType
-import com.mineinabyss.blocky.components.BlockyBlock
 import com.mineinabyss.blocky.components.BlockyInfo
+import com.mineinabyss.geary.datatypes.family.MutableFamilyOperations.Companion.has
 import com.mineinabyss.geary.datatypes.family.family
 import com.mineinabyss.geary.prefabs.PrefabKey
 import com.mineinabyss.geary.prefabs.configuration.components.Prefab
 import com.mineinabyss.geary.systems.accessors.TargetScope
 import com.mineinabyss.geary.systems.accessors.get
-import com.mineinabyss.geary.systems.accessors.has
 import com.mineinabyss.geary.systems.query.GearyQuery
 import com.mineinabyss.looty.ecs.components.LootyType
 
@@ -24,7 +22,3 @@ object BlockyTypeQuery : GearyQuery() {
 }
 
 val blockyQuery = BlockyTypeQuery.filter { it.entity.has<BlockyInfo>() }.map { it.key.toString() }
-val blockyQueryBlocks =
-    BlockyTypeQuery.filter {
-        it.entity.has<BlockyBlock>() && it.entity.get<BlockyBlock>()!!.blockType != BlockType.GROUND
-    }.map { it.key.toString() }
