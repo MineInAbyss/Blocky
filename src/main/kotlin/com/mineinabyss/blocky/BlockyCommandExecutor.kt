a862dadc443e7123a539d4fb5d028d7520a0ed0c
0ffz <dan.voznyy@gmail.com>
2022-08-07 16:29:05 -0400

    Update geary

--- src/main/kotlin/com/mineinabyss/blocky/BlockyCommandExecutor.kt
+++ src/main/kotlin/com/mineinabyss/blocky/BlockyCommandExecutor.kt
@@ -13,9 +13,6 @@ import com.mineinabyss.idofront.commands.extensions.actions.playerAction
 import com.mineinabyss.idofront.messaging.error
 import com.mineinabyss.idofront.messaging.success
 import com.mineinabyss.looty.LootyFactory
-import com.mineinabyss.looty.ecs.components.itemcontexts.PlayerInventorySlotContext
-import com.mineinabyss.looty.ecs.components.itemcontexts.useWithLooty
-import com.mineinabyss.looty.loadItem
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.command.TabCompleter
@@ -40,9 +37,6 @@ class BlockyCommandExecutor : IdofrontCommandExecutor(), TabCompleter {
                         return@playerAction
                     }
 
-                    item.useWithLooty {
-                        PlayerInventorySlotContext(player, slot).loadItem(this)
-                    }
                     player.inventory.setItem(slot, item)
                 }
             }
@@ -69,9 +63,6 @@ class BlockyCommandExecutor : IdofrontCommandExecutor(), TabCompleter {
                             return@playerAction
                         }
 
-                        item.useWithLooty {
-                            PlayerInventorySlotContext(player, slot).loadItem(this)
-                        }
                         player.inventory.setItem(slot, item)
                     }
                 }
@@ -113,17 +104,20 @@ class BlockyCommandExecutor : IdofrontCommandExecutor(), TabCompleter {
                             blockyQuery.filter {
                                 it.startsWith(args[1]) || it.replace("mineinabyss:", "").startsWith(args[1])
                             }
+
                         "modelengine" -> listOf("give", "remove")
                         "menu" -> emptyList()
                         else -> emptyList()
                     }
                 }
+
                 3 -> {
                     when (args[0]) {
                         "modelengine" -> blockyModelEngineQuery
                         else -> emptyList()
                     }
                 }
+
                 else -> emptyList()
             }
         } else emptyList()
--- src/main/kotlin/com/mineinabyss/blocky/BlockyTypeQuery.kt
+++ src/main/kotlin/com/mineinabyss/blocky/BlockyTypeQuery.kt
@@ -3,12 +3,10 @@ package com.mineinabyss.blocky
 import com.mineinabyss.blocky.BlockyTypeQuery.key
 import com.mineinabyss.blocky.components.BlockyInfo
 import com.mineinabyss.blocky.components.BlockyModelEngine
-import com.mineinabyss.geary.datatypes.family.MutableFamilyOperations.Companion.has
 import com.mineinabyss.geary.datatypes.family.family
 import com.mineinabyss.geary.prefabs.PrefabKey
 import com.mineinabyss.geary.prefabs.configuration.components.Prefab
 import com.mineinabyss.geary.systems.accessors.TargetScope
-import com.mineinabyss.geary.systems.accessors.get
 import com.mineinabyss.geary.systems.query.GearyQuery
 import com.mineinabyss.looty.ecs.components.LootyType
 
