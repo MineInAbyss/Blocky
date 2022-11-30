7496f22cba7f5634a96ecc4697b4efb9d1a79000
Boy <sivertpaulsen2@gmail.com>
2022-11-30 15:46:57 +0100

    fix menus

--- src/main/kotlin/com/mineinabyss/blocky/menus/BlockyFurnitureMenu.kt
+++ src/main/kotlin/com/mineinabyss/blocky/menus/BlockyFurnitureMenu.kt
@@ -2,9 +2,8 @@ package com.mineinabyss.blocky.menus
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import com.mineinabyss.blocky.api.BlockyFurnitures.isModelEngineFurniture
-import com.mineinabyss.blocky.systems.BlockyBlockQuery
 import com.mineinabyss.blocky.systems.BlockyBlockQuery.prefabKey
+import com.mineinabyss.blocky.systems.BlockyFurnitureQuery
 import com.mineinabyss.guiy.components.Grid
 import com.mineinabyss.guiy.modifiers.Modifier
 import com.mineinabyss.guiy.modifiers.at
@@ -14,9 +13,7 @@ import com.mineinabyss.guiy.modifiers.size
 fun BlockyUIScope.BlockyFurnitureMenu() {
     Grid(Modifier.size(9, 5)) {
         remember {
-            BlockyBlockQuery.filter {
-                it.entity.isModelEngineFurniture
-            }
+            BlockyFurnitureQuery
         }.sortedBy { it.prefabKey.key }.forEach { HandleMenuClicks(it.prefabKey, player) }
     }
     BackButton(Modifier.at(0, 5))
--- src/main/kotlin/com/mineinabyss/blocky/menus/BlockyNoteBlockMenu.kt
+++ src/main/kotlin/com/mineinabyss/blocky/menus/BlockyNoteBlockMenu.kt
@@ -2,10 +2,11 @@ package com.mineinabyss.blocky.menus
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import com.mineinabyss.blocky.components.core.BlockyBlock
 import com.mineinabyss.blocky.components.core.BlockyBlock.BlockType
+import com.mineinabyss.blocky.components.features.BlockyDirectional
 import com.mineinabyss.blocky.systems.BlockyBlockQuery
 import com.mineinabyss.blocky.systems.BlockyBlockQuery.prefabKey
+import com.mineinabyss.blocky.systems.BlockyBlockQuery.type
 import com.mineinabyss.guiy.components.Grid
 import com.mineinabyss.guiy.modifiers.Modifier
 import com.mineinabyss.guiy.modifiers.at
@@ -16,7 +17,8 @@ fun BlockyUIScope.BlockyNoteBlockMenu() {
     Grid(Modifier.size(9, 5)) {
         remember {
             BlockyBlockQuery.filter {
-                it.entity.get<BlockyBlock>()?.blockType == BlockType.NOTEBLOCK
+                it.type.blockType !in setOf(BlockType.WIRE, BlockType.CAVEVINE) &&
+                it.entity.get<BlockyDirectional>()?.isParentBlock != false
             }
         }.sortedBy { it.prefabKey.key }.forEach { HandleMenuClicks(it.prefabKey, player) }
     }
