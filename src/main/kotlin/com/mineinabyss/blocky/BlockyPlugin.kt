4bff6a924d066a37d12025e3b0d4767a6dfe2245
Boy <sivertpaulsen2@gmail.com>
2023-01-08 17:55:18 +0100

    fix nms

--- src/main/kotlin/com/mineinabyss/blocky/BlockyPlugin.kt
+++ src/main/kotlin/com/mineinabyss/blocky/BlockyPlugin.kt
@@ -18,7 +18,10 @@ import com.mineinabyss.idofront.plugin.listeners
 import com.sk89q.worldedit.WorldEdit
 import com.sk89q.worldedit.bukkit.WorldEditPlugin
 import it.unimi.dsi.fastutil.ints.IntArrayList
+import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.tags.BlockTags
+import net.minecraft.world.item.Item
 import org.bukkit.Instrument
 import org.bukkit.Material
 import org.bukkit.Note
@@ -84,26 +87,25 @@ class BlockyPlugin : JavaPlugin() {
 
     fun runStartupFunctions() {
         blockMap = createBlockMap()
-        //registryTagMap = createTagRegistryMap()
+        registryTagMap = createTagRegistryMap()
         ResourcepackGeneration().generateDefaultAssets()
         MoreCreativeTabsGeneration().generateModAssets()
     }
 
-    //TODO Fix this with new Registry::class methods?
-    /*private fun createTagRegistryMap(): Map<ResourceLocation, IntArrayList> {
-        val map = Registry.BLOCK.tags.map { pair ->
+    private fun createTagRegistryMap(): Map<ResourceLocation, IntArrayList> {
+        val map = BuiltInRegistries.BLOCK.tags.map { pair ->
             pair.first.location to IntArrayList(pair.second.size()).apply {
                 // If the tag is MINEABLE_WITH_AXE, don't add noteblock
                 if (pair.first.location == BlockTags.MINEABLE_WITH_AXE.location) {
                     pair.second.filter {
                         Item.BY_BLOCK[it.value()].toString() != "note_block"
-                    }.forEach { add(Registry.BLOCK.getId(it.value())) }
-                } else pair.second.forEach { add(Registry.BLOCK.getId(it.value())) }
+                    }.forEach { add(BuiltInRegistries.BLOCK.getId(it.value())) }
+                } else pair.second.forEach { add(BuiltInRegistries.BLOCK.getId(it.value())) }
             }
         }.toList().toMap()
 
         return map
-    }*/
+    }
 
     private fun createBlockMap(): Map<BlockData, Int> {
         val blockMap = mutableMapOf<BlockData, Int>()
--- src/main/kotlin/com/mineinabyss/blocky/listeners/BlockyNMSListener.kt
+++ src/main/kotlin/com/mineinabyss/blocky/listeners/BlockyNMSListener.kt
@@ -3,11 +3,11 @@ package com.mineinabyss.blocky.listeners
 import com.mineinabyss.blocky.blockyConfig
 import com.mineinabyss.blocky.registryTagMap
 import it.unimi.dsi.fastutil.ints.IntList
-import net.minecraft.core.Registry
+import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.network.protocol.game.ClientboundUpdateTagsPacket
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.tags.TagNetworkSerialization
-import org.bukkit.craftbukkit.v1_19_R1.entity.CraftPlayer
+import org.bukkit.craftbukkit.v1_19_R2.entity.CraftPlayer
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -18,7 +18,7 @@ class BlockyNMSListener : Listener {
     @EventHandler(priority = EventPriority.MONITOR)
     fun PlayerJoinEvent.removeDefaultTools() {
         if (!blockyConfig.noteBlocks.isEnabled) return
-        val packet = ClientboundUpdateTagsPacket(mapOf(Registry.BLOCK_REGISTRY to createPayload(registryTagMap)))
+        val packet = ClientboundUpdateTagsPacket(mapOf(BuiltInRegistries.BLOCK.key() to createPayload(registryTagMap)))
         (player as CraftPlayer).handle.connection.send(packet)
     }
 
