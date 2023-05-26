375ce804d78a9a14a18eabfa1bf9c64078fb3187
Boy <sivertpaulsen2@gmail.com>
2023-05-26 21:28:29 +0200

    minor config cleanup

--- src/main/kotlin/com/mineinabyss/blocky/BlockyConfig.kt
+++ src/main/kotlin/com/mineinabyss/blocky/BlockyConfig.kt
@@ -20,48 +20,21 @@ data class BlockyConfig(
         val defaultMenu: BlockyMenu = BlockyMenu(height = 5),
         val blockMenu: BlockyMenu = BlockyMenu(),
         val wireMenu: BlockyMenu = BlockyMenu(),
-        val furnitureMenu: BlockyMenu = BlockyMenu(),
+        val furnitureMenu: BlockyMenu = BlockyMenu()
     )
 
-    @Serializable
-    data class BlockyMenu(val title: String = "", val height: Int = 6)
-
-    @Serializable
-    data class MoreCreativeTabsModConfig(
-        val generateJsonForMod: Boolean = false,
-    )
-
-    @Serializable
-    data class BlockyNoteBlockConfig(
-        val isEnabled: Boolean = true,
-        val restoreFunctionality: Boolean = false,
-    )
-
-    @Serializable
-    data class BlockyTripwireConfig(
-        val isEnabled: Boolean = true,
-    )
-
+    @Serializable data class BlockyMenu(val title: String = "", val height: Int = 6)
+    @Serializable data class MoreCreativeTabsModConfig(val generateJsonForMod: Boolean = false)
+    @Serializable data class BlockyNoteBlockConfig(val isEnabled: Boolean = true, val restoreFunctionality: Boolean = false)
+    @Serializable data class BlockyTripwireConfig(val isEnabled: Boolean = true)
+    @Serializable data class BlockyCaveVineConfig(val isEnabled: Boolean = false)
+    @Serializable data class BlockySlabConfig(val isEnabled: Boolean = false)
+    @Serializable data class BlockyStairConfig(val isEnabled: Boolean = false)
     @Serializable
     data class BlockyLeafConfig(
         val isEnabled: Boolean = true,
         val disableBurnForBlockyLeaves: Boolean = false,
         val disableAllLeafDecay: Boolean = false,
-        val shouldReserveOnePersistentLeafPerType: Boolean = true, // if true 54 leaf blocks else 63 leaf blocks
-    )
-
-    @Serializable
-    data class BlockyCaveVineConfig(
-        val isEnabled: Boolean = false,
-    )
-
-    @Serializable
-    data class BlockySlabConfig(
-        val isEnabled: Boolean = false,
-    )
-
-    @Serializable
-    data class BlockyStairConfig(
-        val isEnabled: Boolean = false,
+        val shouldReserveOnePersistentLeafPerType: Boolean = true // if true 54 leaf blocks else 63 leaf blocks
     )
 }
